2e91e205cbcfe82b6f86a5326d0c45d57217a72b
dzakyadlh <106021129+dzakyadlh@users.noreply.github.com>
2023-11-17 23:28:04 +0700

    initial push

---
+++ app/src/main/java/com/nutrifacts/app/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.nutrifacts.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.nutrifacts.app.ui.theme.NutrifactsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NutrifactsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
